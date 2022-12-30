3080b18fa2a68b2346bc9c6439579d308f2f5ae3
Sachin Chaulagai <50958807+chaulags@users.noreply.github.com>
2022-12-30 10:57:33 +0545

    Create minikube_script.sh

---
+++ Bash/Minikube/minikube_script.sh
@@ -0,0 +1,22 @@
+# Update package manager
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install docker.io
+
+# Install the latest version of Minikube
+curl -sSL https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+    -o /usr/local/bin/minikube
+
+# Make the binary executable
+chmod +x /usr/local/bin/minikube
+
+# Install kubectl
+curl -sSL https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl \
+    -o /usr/local/bin/kubectl
+
+# Make the binary executable
+chmod +x /usr/local/bin/kubectl
+
+# Start Minikube
+minikube start --driver=docker
